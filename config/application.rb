4100184573d6f232e350ad4660eb066dcbd08050
Zogoo <chtsogbadrakh@gmail.com>
2021-01-27 00:32:46 +0900

    Add webpacker and vue

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,9 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+# Load .env file
+Dotenv::Railtie.load
+
 module ShopBasket
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
